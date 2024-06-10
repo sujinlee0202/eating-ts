6ff5897aaca82e85794fafe1549919e441e290df
titch <sujinlee0202@gmail.com>
2024-06-10 17:55:51 +0900

    좌표가 잘못 되었을 경우 기본 좌표로 이동하도록 수정

--- src/components/MapSection/MapSection.tsx
+++ src/components/MapSection/MapSection.tsx
@@ -1,19 +1,75 @@
-import useMaps from "../../hooks/useMaps";
-import { NaverMap } from "../../types/naver-map";
+import useMaps, { INITIAL_CENTER, INITIAL_ZOOM } from "../../hooks/useMaps";
+import { Coordinates, NaverMap } from "../../types/naver-map";
 import Sidebar from "../Sidebar/Sidebar";
-import Map from "./Map";
 import Markers from "./Markers";
+import styles from "./MapSection.module.css";
+import { useEffect, useRef } from "react";
+import { getMapCetner } from "../../api/naver/map";
+import { locationPermissionError } from "../../messages/alertMessages";
 
 const MapSection = () => {
+  const mapRef = useRef<NaverMap | null>(null);
   const { initializeMap } = useMaps();
 
-  const onLoadMap = (map: NaverMap) => {
-    initializeMap(map);
-  };
+  useEffect(() => {
+    const onLoadMap = (map: NaverMap) => {
+      initializeMap(map);
+    };
+
+    const loadMap = (initialCenter: Coordinates) => {
+      if (mapRef.current) return null;
+
+      const mapOptions = {
+        center: getMapCetner(initialCenter),
+        zoom: INITIAL_ZOOM,
+        minZoom: 9,
+        scaleControl: false,
+        mapDataControl: false,
+        logoControlOptions: {
+          position: naver.maps.Position.BOTTOM_LEFT,
+        },
+      };
+
+      const map = new naver.maps.Map("map", mapOptions);
+      mapRef.current = map;
+
+      if (onLoadMap) {
+        onLoadMap(map);
+      }
+    };
+
+    const currentGeoLocation = () => {
+      const success = (pos: GeolocationPosition) => {
+        const { latitude, longitude } = pos.coords;
+        const currentPosition = new window.naver.maps.LatLng(
+          latitude,
+          longitude
+        );
+        if (currentPosition.x < 0 || currentPosition.y < 0) {
+          loadMap(INITIAL_CENTER);
+        } else {
+          loadMap([currentPosition.y, currentPosition.x]);
+        }
+      };
+
+      const error = () => {
+        alert(locationPermissionError);
+        loadMap(INITIAL_CENTER);
+      };
+
+      navigator.geolocation.getCurrentPosition(success, error, {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 5000,
+      });
+    };
+
+    currentGeoLocation();
+  }, [initializeMap]);
 
   return (
     <>
-      <Map onLoadMap={onLoadMap} />
+      <div id='map' className={styles.container} />
       <Markers />
       <Sidebar />
     </>
