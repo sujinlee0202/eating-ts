061376f57d2065adbc33f997b725fa0621a61852
titch <sujinlee0202@gmail.com>
2024-06-11 19:56:30 +0900

    sidebar 컴포넌트명 수정

--- src/components/MapSection/MapSection.tsx
+++ src/components/MapSection/MapSection.tsx
@@ -1,6 +1,6 @@
 import useMaps, { INITIAL_CENTER, INITIAL_ZOOM } from "../../hooks/useMaps";
 import { Coordinates, NaverMap } from "../../types/naver-map";
-import Sidebar from "../Sidebar/Sidebar.tsx";
+import Sidebar from "../Sidebar/Sidebar";
 import styles from "./MapSection.module.css";
 import { useEffect, useRef } from "react";
 import { getMapCetner } from "../../api/naver/map";
